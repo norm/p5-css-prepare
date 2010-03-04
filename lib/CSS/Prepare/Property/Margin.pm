55ad972da8e1d61a97c9559d620afeaf7251f11f
Mark Norman Francis <norm@cackhanded.net>
2010-03-04 16:41:32 +0000

    Refactor top/left/bottom/right shorthand value expansions into one included module, rather than having it copied everywhere it is used.

--- lib/CSS/Prepare/Property/Margin.pm
+++ lib/CSS/Prepare/Property/Margin.pm
@@ -1,6 +1,7 @@
 package CSS::Prepare::Property::Margin;
 
 use Modern::Perl;
+use CSS::Prepare::Property::Expansions;
 
 
 
@@ -13,53 +14,15 @@ sub parse {
     
     given ( $property ) {
         when ( 'margin' ) {
-            my @values = split( m{\s+}, $value );
-            
-            given ( $#values ) {
-                when ( 0 ) {
-                    # top/bottom/left/right shorthand
-                    foreach my $subproperty qw( top bottom left right ) {
-                        $canonical{"margin-$subproperty"} = $values[0];
-                    }
-                }
-                when ( 1 ) {
-                    # top/bottom and left/right shorthand
-                    foreach my $subproperty qw ( top bottom ) {
-                        $canonical{"margin-$subproperty"} = $values[0];
-                    }
-                    foreach my $subproperty qw ( left right ) {
-                        $canonical{"margin-$subproperty"} = $values[1];
-                    }
-                }
-                when ( 2 ) {
-                    # top, left/right and bottom shorthand
-                    $canonical{"margin-top"}    = $values[0];
-                    $canonical{"margin-bottom"} = $values[2];
-                    foreach my $subproperty qw ( left right ) {
-                        $canonical{"margin-$subproperty"} = $values[1];
-                    }
-                }
-                when ( 3 ) {
-                    # top, right, bottom and left shorthand
-                    $canonical{"margin-top"}    = $values[0];
-                    $canonical{"margin-bottom"} = $values[2];
-                    $canonical{"margin-left"}   = $values[3];
-                    $canonical{"margin-right"}  = $values[1];
-                }
-            }
+            %canonical = expand_tlbr_shorthand(
+                    'margin-%s',
+                    $value
+                );
         }
-        when ( 'margin-top' ) {
-            $canonical{ $property } = $value;
-        }
-        when ( 'margin-bottom' ) {
-            $canonical{ $property } = $value;
-        }
-        when ( 'margin-left' ) {
-            $canonical{ $property } = $value;
-        }
-        when ( 'margin-right' ) {
-            $canonical{ $property } = $value;
-        }
+        when ( 'margin-top'    ) { $canonical{ $property } = $value; }
+        when ( 'margin-bottom' ) { $canonical{ $property } = $value; }
+        when ( 'margin-left'   ) { $canonical{ $property } = $value; }
+        when ( 'margin-right'  ) { $canonical{ $property } = $value; }
     }
     
     return %canonical;
--- lib/CSS/Prepare/Property/Padding.pm
+++ lib/CSS/Prepare/Property/Padding.pm
@@ -1,6 +1,7 @@
 package CSS::Prepare::Property::Padding;
 
 use Modern::Perl;
+use CSS::Prepare::Property::Expansions;
 
 
 
@@ -13,53 +14,15 @@ sub parse {
     
     given ( $property ) {
         when ( 'padding' ) {
-            my @values = split( m{\s+}, $value );
-            
-            given ( $#values ) {
-                when ( 0 ) {
-                    # top/bottom/left/right shorthand
-                    foreach my $subproperty qw( top bottom left right ) {
-                        $canonical{"padding-$subproperty"} = $values[0];
-                    }
-                }
-                when ( 1 ) {
-                    # top/bottom and left/right shorthand
-                    foreach my $subproperty qw ( top bottom ) {
-                        $canonical{"padding-$subproperty"} = $values[0];
-                    }
-                    foreach my $subproperty qw ( left right ) {
-                        $canonical{"padding-$subproperty"} = $values[1];
-                    }
-                }
-                when ( 2 ) {
-                    # top, left/right and bottom shorthand
-                    $canonical{"padding-top"}    = $values[0];
-                    $canonical{"padding-bottom"} = $values[2];
-                    foreach my $subproperty qw ( left right ) {
-                        $canonical{"padding-$subproperty"} = $values[1];
-                    }
-                }
-                when ( 3 ) {
-                    # top, right, bottom and left shorthand
-                    $canonical{"padding-top"}    = $values[0];
-                    $canonical{"padding-bottom"} = $values[2];
-                    $canonical{"padding-left"}   = $values[3];
-                    $canonical{"padding-right"}  = $values[1];
-                }
-            }
+            %canonical = expand_tlbr_shorthand(
+                    'padding-%s',
+                    $value
+                );
         }
-        when ( 'padding-top' ) {
-            $canonical{ $property } = $value;
-        }
-        when ( 'padding-bottom' ) {
-            $canonical{ $property } = $value;
-        }
-        when ( 'padding-left' ) {
-            $canonical{ $property } = $value;
-        }
-        when ( 'padding-right' ) {
-            $canonical{ $property } = $value;
-        }
+        when ( 'padding-top'    ) { $canonical{ $property } = $value; }
+        when ( 'padding-bottom' ) { $canonical{ $property } = $value; }
+        when ( 'padding-left'   ) { $canonical{ $property } = $value; }
+        when ( 'padding-right'  ) { $canonical{ $property } = $value; }
     }
     
     return %canonical;
